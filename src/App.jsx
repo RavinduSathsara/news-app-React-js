c2cad43ec9b1fe1968ea6ad8d7bb140d792b3c51
Ravindu_Sathsara <rsathsara330@gmail.com>
2021-11-18 23:29:58 +0530

    Add Post to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import AppNavBar from "./components/AppNavBar";
+import RecipeReviewCard from "./components/RecipeReviewCard";
+import { Container } from "@mui/material";
 
 const App = () => {
   return (
     <div>
       <AppNavBar />
+      <Container maxWidth="md">
+        <RecipeReviewCard />
+      </Container>
     </div>
   );
 };
