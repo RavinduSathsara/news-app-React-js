d10b9ce3c4aeeda9cd08eca05990d5b93c5afdd0
Ravindu_Sathsara <rsathsara330@gmail.com>
2021-11-18 23:07:04 +0530

    Add NavBar to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import AppNavBar from "./components/AppNavBar";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <AppNavBar />
     </div>
   );
 };
