fa8d4ff9f84908effaa34e6014936c3698e1da49
Ravindu_Sathsara <rsathsara330@gmail.com>
2021-11-19 12:59:32 +0530

    Clean the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container } from "@mui/material";
-import AppNavBar from "./components/AppNavBar";
 
 import "./index.css";
 import TimeLinePost from "./components/TimeLinePost";
@@ -9,7 +8,6 @@ import PrimarySearchAppBar from "./components/PrimarySearchAppBar";
 const App = () => {
   return (
     <div>
-      {/* <AppNavBar /> */}
       <PrimarySearchAppBar />
       <Container maxWidth="md">
         <TimeLinePost />
--- src/components/PrimarySearchAppBar.jsx
+++ src/components/PrimarySearchAppBar.jsx
@@ -173,7 +173,7 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: "none", sm: "block" } }}
           >
-            MUI
+            News App
           </Typography>
           <Search>
             <SearchIconWrapper>
